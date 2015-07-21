f8769d17e83ad259fa92463ae94c0df755881c07
SophieV <Sophie@adoptive.co>
2015-07-21 13:55:07 -0400

    reactjs setup

---
+++ src/js/Parent.js
@@ -0,0 +1,16 @@
+/** @jsx React.DOM */
+var React = require('react');
+var Child = require('./Child');
+
+var Parent = React.createClass({
+  render: function(){
+    return (
+      <div>
+        <div> This is the parent. </div>
+        <Child name="child"/>
+      </div>
+    )
+  }
+});
+
+module.exports = Parent;
